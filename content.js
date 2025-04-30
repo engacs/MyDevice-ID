e9b1b7462b428d48965c7707a931a5007f373d47
Yahya Saddiq <yahya.saddiq@gmail.com>
2025-04-30 16:14:36 +0300

    add device_name to the reponse

--- content.js
+++ content.js
@@ -4,12 +4,13 @@ window.addEventListener("message", (event) => {
   if (event.source !== window || event.data.type !== "GET_MY_DEVICE_ID") return;
 
   // Get the device ID from chrome.storage.local
-  chrome.storage.local.get("my_device_id", (data) => {
     // Send the device ID back to the webpage (your Vue app)
+  chrome.storage.local.get(["my_device_id", "my_device_name"], (data) => {
     window.postMessage(
       {
         type: "MY_DEVICE_ID_RESPONSE",
         my_device_id: data.my_device_id || null,
+        my_device_name: data.my_device_name || null,
       },
       "*"
     );
