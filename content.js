afad0df78fcd3f823b1d1b65f6c82a0b79a73254
Yahya Saddiq <yahya.saddiq@gmail.com>
2025-04-30 16:20:57 +0300

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,6 @@
-// Listen for messages from the webpage (your Vue app)
 window.addEventListener("message", (event) => {
-  // Ensure the message is coming from your Vue app
   if (event.source !== window || event.data.type !== "GET_MY_DEVICE_ID") return;
 
-  // Get the device ID from chrome.storage.local
-    // Send the device ID back to the webpage (your Vue app)
   chrome.storage.local.get(["my_device_id", "my_device_name"], (data) => {
     window.postMessage(
       {
